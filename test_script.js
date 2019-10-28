584f4a93df6fd39cf9f5dd5631322616143141c7
Karam Abuaisha <karam_abuaisha@hotmail.com>
2019-10-28 18:08:24 -0400

    Update test_script.js

--- test_script.js
+++ test_script.js
@@ -7,8 +7,10 @@ Map.centerObject(roi, 8)
 //Load Sentinel-1 SAR collection and filter according to data collection type
 var S1 = ee.ImageCollection('COPERNICUS/S1_GRD')
   .filterBounds(roi)
-  .filterDate('2016-07-01','2016-11-01')
-  .filter(ee.Filter.listContains('transmitterReceiverPolarisation', 'VV'))
+  .filterDate('2019-07-01','2019-10-31')
+  .filter(ee.Filter.listContains('transmitterReceiverPolarisation', 'HH'))
+  .filter(ee.Filter.listContains('transmitterReceiverPolarisation', 'HV'))
+  .filter(ee.Filter.eq('instrumentMode', 'EW'))
 
 // //Add first image to map to get an idea of what a SAR image looks like  
 // Map.addLayer(S1.first(),{bands: 'VV',min: -18, max: 0}, 'SAR image')
